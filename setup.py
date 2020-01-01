0431a8ff00a1b50226adcf5ff7bb6b15f8d21b0d
BillMills <mills.wj@gmail.com>
2019-12-31 20:35:44 -0500

    first post

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools, subprocess
+from distutils.command.install import install as _install
+
+class install(_install):
+    def run(self):
+        subprocess.call(['pwd'])
+        subprocess.call(['make', 'clean', '-C', 'cqc'])
+        subprocess.call(['make', '-C', 'cqc'])
+        _install.run(self)
+
+setuptools.setup(
+    name="cqc",
+    version="0.0.18",
+    packages=setuptools.find_packages(),
+    package_data={'cqc': ['cqc/cqc.so']},
+    include_package_data=True,
+    cmdclass={'install': install},
+)
